a3c16bdbbd137b90cd89117f18f5359d3d467376
thek4n <djvlad967891@gmail.com>
2021-11-15 00:34:57 +0300

    7

--- src/7_dict_to_object.py
+++ src/7_dict_to_object.py
@@ -1,24 +1,26 @@
 class MyObject:
 
-    def from_dict(self, d):
+    def _set_attrs(self, d: dict):
         for x, y in d.items():
             setattr(self, x, MyObject.from_dict(self, y) if isinstance(y, dict) else y)
         return self
 
+    def from_dict(self, d):
+        return self._set_attrs(d)
+
     def from_params(self, **kwargs):
-        for x, y in kwargs.items():
-            setattr(self, x, MyObject.from_dict(self, y) if isinstance(y, dict) else y)
-        return self
+        return self._set_attrs(kwargs)
 
 
 if __name__ == '__main__':
 
-    data = {'a': 5, 'b': 7, 'c': {'d': 8}}
+    data1 = {'a': 5, 'b': 7, 'c': {'d': 8}}
+    data2 = {'a': 5, 'b': 7, 'c': {'d': 9}, "new": {"some_list": list(range(10))}}
 
     ob = MyObject()
-    ob.from_dict(data)
+    ob.from_dict(data1)
     print(ob.c.d)
 
     ob2 = MyObject()
-    ob2.from_params(**data)
-    print(ob2.c.d)
+    ob2.from_params(**data2)
+    print(ob2.new.some_list)
