30ce145d86495c119a7f3a91c7ddeaaaeb9252f9
thek4n <djvlad967891@gmail.com>
2021-11-18 01:09:01 +0300

    upd: 7_dict_to_object.py

--- src/7_dict_to_object.py
+++ src/7_dict_to_object.py
@@ -19,7 +19,7 @@ class MyObject:
         self.__dict__.clear()
 
     def __str__(self):
-        return f'{self.__class__.__name__}({self.__dict__})'
+        return f'{self.__class__.__name__}<{self.__dict__}>'
 
 
 if __name__ == '__main__':
