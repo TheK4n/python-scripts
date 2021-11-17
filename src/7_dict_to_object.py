229619d169a9e101e0c72fdeeb972505906b50db
thek4n <djvlad967891@gmail.com>
2021-11-18 01:01:33 +0300

    upd: 7_dict_to_object.py

--- src/7_dict_to_object.py
+++ src/7_dict_to_object.py
@@ -1,3 +1,4 @@
+
 class MyObject:
 
     def __init__(self, d: dict = None):
@@ -22,14 +23,8 @@ class MyObject:
 
 
 if __name__ == '__main__':
-    data1 = {'a': 5, 'b': 7, 'c': {'d': 8}}
-    data2 = {'a': 5, 'b': 7, 'c': {'d': 9}, "new": {"some_list": list(range(10))}}
+    data = {'a': 5, 'b': 7, 'c': {'c1': 9}, "d": {"d1": list(range(10))}}
 
     ob = MyObject()
-    ob.from_dict(data1)
-    print(ob.c.d)
-
-    ob2 = MyObject()
-    ob2.from_params(**data2)
-    print(ob2.new.some_list)
-    print(ob2)
+    ob.from_params(**data)
+    print(ob.d.d1)
