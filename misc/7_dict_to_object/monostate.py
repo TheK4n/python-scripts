9ac2560b7725af4d65a320822d8c99a72a9de412
TheK4n <djvlad967891@gmail.com>
2021-12-03 04:14:40 +0300

    upd

--- misc/7_dict_to_object/monostate.py
+++ misc/7_dict_to_object/monostate.py
@@ -26,4 +26,4 @@ if __name__ == '__main__':
     assert b1 is not b2  # are different instances
 
     assert b1 is not b1.b  # is internal instance is different from external
-    assert b1.a is b1.b.a  # is internal instance is monostate with external
+    assert b1.__dict__ is b1.b.__dict__  # is internal instance is monostate with external
