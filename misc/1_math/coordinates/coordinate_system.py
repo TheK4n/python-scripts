66ab53b3e2103a313cc4a6b12449f39214193c61
TheK4n <djvlad967891@gmail.com>
2022-01-11 04:33:38 +0300

    ref coordinate

--- misc/1_math/coordinates/coordinate_system.py
+++ misc/1_math/coordinates/coordinate_system.py
@@ -1,80 +1,80 @@
 import math
+from abc import ABC
 
 
-class Coord(float):
+class Coordinate(float):
     def __str__(self):
         return str(round(self, 3))
 
 
-class Phi(Coord):
+class Phi(Coordinate):
 
     def __init__(self, value: int | float):
-        self.__phi = Coord(value)
+        self.__phi = Coordinate(value)
 
     @property
-    def degrees(self) -> Coord:
-        return Coord(math.degrees(self.__phi) % 360)
+    def degrees(self) -> Coordinate:
+        return Coordinate(math.degrees(self.__phi) % 360)
 
 
-class Cartesian:
+class CoordinatesBase(ABC):
+    attr_names = "", ""
 
     def __init__(self, x: float | int, y: float | int):
         self.set(x, y)
 
     def set(self, x: float | int, y: float | int):
-        self.__x, self.__y = Coord(x), Coord(y)
+        self._xx, self._yy = Coordinate(x), Coordinate(y)
 
-    def get(self) -> (Coord, Coord):
-        return self.__x, self.__y
+    def get(self) -> (Coordinate, Coordinate):
+        return self._xx, self._yy
+
+    def __str__(self):
+        x, y = attr_names
+        return f"{self.__class__.__name__}<({x}={self._xx}, {y}={self._yy})>"
+
+
+class Cartesian(CoordinatesBase):
+    attr_names = "x", "y"
 
     def create_polar(self) -> "Polar":
-        return Polar(*self.__cart2pol())
+        return Polar(*self.__convert_to_polar())
 
-    def __cart2pol(self) -> (float, float):
-        x, y = self.__x, self.__y
+    def __convert_to_polar(self) -> (float, float):
+        x, y = self._xx, self._yy
         phi = math.atan2(y, x)
         phi = math.pi * 2 + phi if phi < 0 else phi
         return math.sqrt(x * x + y * y), phi
 
-    def __str__(self):
-        return f"{self.__class__.__name__}<(x={self.__x}, y={self.__y})>"
+    @property
+    def x(self) -> Coordinate:
+        return self._xx
 
     @property
-    def x(self) -> Coord:
-        return self.__x
-
-    @property
-    def y(self) -> Coord:
-        return self.__y
+    def y(self) -> Coordinate:
+        return self._yy
 
 
-class Polar:
-    def __init__(self, rho: float | int, phi: float | int):
-        self.set(rho, phi)
-
-    def set(self, rho: float | int, phi: float | int):
-        self.__rho, self.__phi = Coord(rho), Phi(phi)
-
-    def get(self) -> (Coord, Phi):
-        return self.__rho, self.__phi
+class Polar(CoordinatesBase):
+    attr_name = "rho", "phi"
 
     def create_cartesian(self) -> "Cartesian":
-        return Cartesian(*self.__pol2cart())
+        return Cartesian(*self.__convert_to_cart())
 
-    def __pol2cart(self) -> (float, float):
-        rho, phi = self.__rho, self.__phi
+    def __convert_to_cart(self) -> (float, float):
+        rho, phi = self._xx, self._yy
         return rho * math.cos(phi), rho * math.sin(phi)
 
-    def __str__(self):
-        return f"{self.__class__.__name__}<(rho={self.__rho}, phi={self.__phi})>"
+    def set(self, x: float | int, y: float | int):
+        self._xx, self._yy = Coordinate(x), Phi(y)
 
     @property
-    def rho(self) -> Coord:
-        return self.__rho
+    def rho(self) -> Coordinate:
+        return self._xx
 
     @property
     def phi(self) -> Phi:
-        return self.__phi
+        return self._yy
 
 
 if __name__ == '__main__':
