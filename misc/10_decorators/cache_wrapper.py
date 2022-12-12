10bd9f0f8841b74636a99edb8079600c7bdf688c
TheK4n <djvlad967891@gmail.com>
2022-12-12 21:51:38 +0300

    fix cache wrapper
    add to key wrapped func name to prefer cache result for all functions
    with same args

--- misc/10_decorators/cache_wrapper.py
+++ misc/10_decorators/cache_wrapper.py
@@ -1,4 +1,5 @@
 import functools
+from timeit import timeit
 
 
 cached_results = {}
@@ -8,12 +9,36 @@ def cache(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         global cached_results
-        sorted_arguments = tuple(set(args + tuple(kwargs.values())))
+        sorted_arguments_with_func_name = (func.__name__, tuple(sorted(args + tuple(kwargs.values()))))
 
-        if sorted_arguments in cached_results:
-            return cached_results[sorted_arguments]
+        if sorted_arguments_with_func_name in cached_results:
+            return cached_results[sorted_arguments_with_func_name]
         else:
             res = func(*args, **kwargs)
-            cached_results[sorted_arguments] = res
+            cached_results[sorted_arguments_with_func_name] = res
             return res
     return wrapper
+
+
+@cache
+def test(a: float) -> float:
+    for _ in range(100_000):
+        for _ in range(1_000):
+            a += 3.14
+    return a
+
+
+@cache
+def test2(a: int) -> int:
+    for _ in range(100_000):
+        for _ in range(1_000):
+            a += 3
+    return a
+
+
+
+print(timeit("test(1)", number=1, globals=globals()))
+print(timeit("test(1)", number=1, globals=globals()))
+
+print(timeit("test2(1)", number=1, globals=globals()))
+print(timeit("test2(1)", number=1, globals=globals()))
