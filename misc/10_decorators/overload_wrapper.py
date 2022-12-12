1f1cac23237c57e3953a09caace5be3197ecbdac
TheK4n <djvlad967891@gmail.com>
2022-12-12 21:28:08 +0300

    fix overload wrapper

--- misc/10_decorators/overload_wrapper.py
+++ misc/10_decorators/overload_wrapper.py
@@ -1,24 +1,22 @@
 
-from functools import wraps
-
-keys = {}
+KEYS = {}
 
 
 def overload(func):
-    global keys
+    global KEYS
 
     types = tuple(func.__annotations__.values())
     func_key = (func.__name__, types)
-    keys[func_key] = func
+    KEYS[func_key] = func
 
     def inner(*args, **kwargs):
-        global keys
+        global KEYS
 
         arguments = args + tuple(kwargs.values())
         types = tuple(map(type, arguments))
         func_key = (inner.__name__, types)
 
-        saved_func = keys[func_key]
+        saved_func = KEYS[func_key]
         res = saved_func(*args, **kwargs)
 
         return res
@@ -52,8 +50,3 @@ assert append("da", "net") == "danet"
 assert append(24, 2) == 12
 assert append("da ", 3) == "da da da "
 
-print(keys)
-
-
-# names func (func.__name__, (type1, type2))
-# key arguments, other order
