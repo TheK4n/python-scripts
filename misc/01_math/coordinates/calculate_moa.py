6c63d2880cdeb0a1df08d11a0a6a6f59734afa6a
TheK4n <djvlad967891@gmail.com>
2023-01-10 21:10:03 +0300

    add calculate devitation func

--- misc/01_math/coordinates/calculate_moa.py
+++ misc/01_math/coordinates/calculate_moa.py
@@ -1,12 +1,22 @@
 import math
-import sys
 
 
-def calculate_moa(distance, devitation):
+MOA_PER_CLICK = 0.25
+DISTANCE = 275  # meters
+DEVITATION = 40 # centimeter
+
+
+def calculate_moa(distance: float, devitation: float) -> float:
     hypo = math.sqrt(pow(devitation, 2) + pow(distance, 2))
     return math.degrees(math.asin(devitation / hypo)) * 60
 
 
+def calculate_devitation(distance: float, moa: float) -> float:
+    return math.tan(math.radians(moa/60)) * distance
+
+
 if __name__ == "__main__":
-    print(calculate_moa(*map(float, sys.argv[1:3])))
+    print("Distance:", DISTANCE, "meters")
+    print("Clicks:", round(calculate_moa(DISTANCE, DEVITATION / 100) / MOA_PER_CLICK, 3), "by", DEVITATION, "centimeters")
+    print("Devitation by 1 click:", round(calculate_devitation(DISTANCE, 1 * MOA_PER_CLICK) * 100, 3), "centimeters")
 
